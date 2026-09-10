d8a4a4347cac1d6c255c3cd27ccb4d1d2edc22f7
abdulmoeedasad19-max <abdulmoeedasad19@gmail.com>
2026-09-10 19:16:27 +0500

    setting up for hte deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,3 @@
-
 import type { NextConfig } from "next";
 
 const isDev = process.env.NODE_ENV === "development";
@@ -21,50 +20,16 @@ const cspHeader = `
   .trim();
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
     qualities: [75, 90, 100],
+    unoptimized: true,
   },
-
   experimental: {
     serverActions: {
       bodySizeLimit: "6mb",
     },
-  },
-
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader,
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Permissions-Policy",
-            value:
-              "camera=(), microphone=(), geolocation=(), payment=(), usb=(), interest-cohort=()",
-          },
-        ],
-      },
-    ];
-  },
+  }
 };
 
 export default nextConfig;
